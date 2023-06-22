bb41163aa7ce119254b5c3839f87b669488a8f66
Juan Esteban Salazar <juanesteban.salazar1@utp.edu.co>
2023-06-21 22:42:10 -0500

    firts commit

---
+++ primerproyectosprintboot/primerproyectosprintboot/src/main/java/guia/utp/primerproyectosprintboot/primerproyectosprintboot/PrimerproyectosprintbootApplication.java
@@ -0,0 +1,15 @@
+package guia.utp.primerproyectosprintboot.primerproyectosprintboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimerproyectosprintbootApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(PrimerproyectosprintbootApplication.class, args);
+		
+	}
+
+}
---
+++ primerproyectosprintboot/primerproyectosprintboot/src/test/java/guia/utp/primerproyectosprintboot/primerproyectosprintboot/PrimerproyectosprintbootApplicationTests.java
@@ -0,0 +1,13 @@
+package guia.utp.primerproyectosprintboot.primerproyectosprintboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimerproyectosprintbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
